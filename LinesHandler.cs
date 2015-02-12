35ba799dac4d32c0333feb010cf2f861109483c8
kyeong hoon jung <kyeong.jung@gmail.com>
2015-02-11 21:32:07 -0800

    updated touch input to use raycasting to interact with gameobjects

--- LinesHandler.cs
+++ LinesHandler.cs
@@ -3,92 +3,61 @@ using System.Collections;
 
 public class LinesHandler : MonoBehaviour
 {
-	public Color c1 = Color.red;
+	public Color c1 = Color.yellow;
 	public Color c2 = Color.red;
 	
 	private GameObject lineGO;
 	private LineRenderer lineRenderer;
 	private int i = 0;
-
-	//Comment
+	private RaycastHit hit;
 	void Start()
 	{
 		lineGO = new GameObject("Line");
 		lineGO.AddComponent<LineRenderer>();
-		lineGO.AddComponent<Rigidbody> ();
-		lineGO.transform.position.Set (0, 0, 15);
 		lineRenderer = lineGO.GetComponent<LineRenderer>();
-		//lineRenderer.material = new Material(Shader.Instantiate(
+		//lineRenderer.material = new Material(Shader.Find("Mobile/Particles/Additive"));
 		lineRenderer.SetColors(c1, c2);
 		lineRenderer.SetWidth(0.3F, 0);
 		lineRenderer.SetVertexCount(0);
-
 	}
 	
 	void Update()
 	{
-
-			if (Input.GetMouseButtonUp(0)) {
-				//Debug.Log("button up");	
-				
-				lineRenderer.SetVertexCount(0);
-				i = 0;
-				
-				BoxCollider[] lineColliders = lineGO.GetComponents<BoxCollider>();
-				
-				foreach(BoxCollider b in lineColliders)
-				{
-					Destroy(b);
-				} 
-			} else if (Input.GetMouseButton (0))  {
-				//Debug.Log("Button down");
-				lineRenderer.SetVertexCount(i+1);
-				Vector3 mPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15);
-				lineRenderer.SetPosition(i, Camera.main.ScreenToWorldPoint(mPosition));
-				i++;
-				
-				BoxCollider bc = lineGO.AddComponent<BoxCollider>();
-				bc.transform.position = new Vector3(lineRenderer.transform.position.x, lineRenderer.transform.position.y, 15);
-				bc.size.Set(5f, 5f, 5f);
-
-				
-			}
-
-
-		/* if (Input.touchCount > 0)
+		if (Input.touchCount > 0)
 		{
 			Touch touch = Input.GetTouch(0);
 			
 			if(touch.phase == TouchPhase.Moved)
 			{
 				lineRenderer.SetVertexCount(i+1);
-				Vector3 mPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15);
+				Vector3 mPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5);
 				lineRenderer.SetPosition(i, Camera.main.ScreenToWorldPoint(mPosition));
 				i++;
+				//Creating a new ray from the position of the touch.
+				Ray ray = Camera.main.ScreenPointToRay (touch.position);
 				
+				if (Physics.Raycast(ray, out hit)) {
+					Debug.Log("raycast hit!");
+					//on raycast hit, checking tag of collider
+					if(hit.collider.tag == "Enemy")
+					{
+						//calling a public function from the enemy object
+						Enemy e = (Enemy)hit.collider.gameObject.GetComponent(typeof(Enemy));
+						e.OnHit();
+					}
 
-				BoxCollider bc = lineGO.AddComponent<BoxCollider>();
-				bc.transform.position = lineRenderer.transform.position;
-				bc.size = new Vector3(0.1f, 0.1f, 0.1f);
+				}
 			}
 			
 			if(touch.phase == TouchPhase.Ended)
 			{
-
+				/* Remove Line */
+				
 				lineRenderer.SetVertexCount(0);
 				i = 0;
+				
 
-				
-				BoxCollider[] lineColliders = lineGO.GetComponents<BoxCollider>();
-				
-				foreach(BoxCollider b in lineColliders)
-				{
-					Destroy(b);
-				}
 			}
-		} */
+		}
 	}
 }
-
-//Hello World
-
